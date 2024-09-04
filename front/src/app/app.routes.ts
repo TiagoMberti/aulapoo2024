f9921167db0d7ecdd04ad83e58cfb1f6739880ea
Tiago Berti <tiagomelloberti@gmail.com>
2024-09-04 23:01:57 +0000

    Versão Inicial

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+
+export const routes: Routes = [
+    {path:'',component:TelaInicialComponent}
+];
